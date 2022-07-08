eaefc20d960918bbadd465d0a11c1961ab4025d3
高江华 <g598670138@163.com>
2022-07-09 05:09:42 +0800

    修改底部链接

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -7,11 +7,11 @@ const Footer: NextPage = () => {
     return (
         <div className={clsx(styles.footer, 'dflex', 'flexcolumn', 'acenter', 'mt2')}>
             <div className={clsx(styles.urls, 'dflex')}>
-                <Button type="primary" className={clsx(styles.btnn)} target="_blank" href="http://gaojianghua.cn/blog">博客社区</Button>
+                <Button type="primary" className={clsx(styles.btnn)} target="_blank" href="https://gaojianghua.cn/blog">博客社区</Button>
                 <div className={clsx('px2')}></div>
-                <Button type="primary" className={clsx(styles.btnn)} target="_blank" href="http://gaojianghua.cn/docs/">作者文档</Button>
+                <Button type="primary" className={clsx(styles.btnn)} target="_blank" href="https://gaojianghua.cn/docs/">作者文档</Button>
                 <div className={clsx('px2')}></div>
-                <Button type="primary" className={clsx(styles.btnn)} target="_blank" href="http://gaojianghua.cn/">服务官网</Button>
+                <Button type="primary" className={clsx(styles.btnn)} target="_blank" href="https://gaojianghua.cn/">服务官网</Button>
             </div>
             <div className={clsx(styles.text, 'cur')}>邮箱：g598670138@163.com 个人微信号：woshigaojianghua</div>
             <a target="_blank" href='https://beian.miit.gov.cn' className={clsx(styles.text, 'cur')}>Copyright © 2022-present JiangHua Gao 版权所有 浙ICP备2022001576号</a>
