43010516202b284c036f3434a77eeb36c0ce9427
15257184434 <g598670138@163.com>
2023-02-07 17:42:19 +0800

    微调样式

--- components/avatar/index.tsx
+++ components/avatar/index.tsx
@@ -1,3 +1,13 @@
+/*
+ * @Author       : 15257184434 g598670138@163.com
+ * @Date         : 2023-02-06 16:06:53
+ * @LastEditors  : 15257184434 g598670138@163.com
+ * @LastEditTime : 2023-02-07 17:18:12
+ * @FilePath     : \Official-Document\components\avatar\index.tsx
+ * @Description  : 
+ * 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+ */
 import type { NextPage } from 'next';
 import clsx from 'clsx';
 import { Image } from 'antd'
@@ -9,7 +19,7 @@ const Avatar: NextPage<IAvatarProps> = ({className}) => {
         <Image
             rootClassName={clsx(styles.image)}
             preview={false}
-            src="https://gaojianghua.oss-cn-hangzhou.aliyuncs.com/home/gaojianghua.jpg"
+            src="/Wolffy.png"
         />
     </div>);
 };
