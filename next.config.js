853515204a7118915850cdf716661fb1bb504b98
高江华 <g598670138@163.com>
2023-04-09 21:08:29 +0800

    编写刻度

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: "/wolffy",
   basePath: "/wolffy",
-  publicRuntimeConfig: {
-    basePath: "/wolffy",
-  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     config.resolve.alias['C'] = path.resolve(__dirname, './components');
