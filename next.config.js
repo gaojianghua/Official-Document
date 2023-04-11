341f819fb662265eabfa299f07cc5a3eb31ef100
高江华 <g598670138@163.com>
2023-04-11 20:41:34 +0800

    修补部署问题

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output:"standalone",
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     config.resolve.alias['C'] = path.resolve(__dirname, './components');
