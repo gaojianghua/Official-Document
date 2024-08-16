e90c9cad21241dff25d9e39439f9ae24eb86d2b0
高江华 <g598670138@163.com>
2024-08-16 13:55:01 +0800

    更新

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,3 +1,10 @@
+/*
+ * @Author: 高江华 g598670138@163.com
+ * @Date: 2023-06-14 14:48:28
+ * @LastEditors: 高江华
+ * @LastEditTime: 2024-08-16 13:47:04
+ * @Description: file content
+ */
 import type { NextPage } from 'next';
 import styles from './index.module.scss';
 import clsx from 'clsx';
@@ -9,11 +16,11 @@ const Card: NextPage<ICardProps> = (props) => {
     const { item, isManagement, openDelete, editorMark } = props;
     return (
         <div style={{ backgroundImage: 'url(' + `${item.image_bg}` + ')' }}
-             className={clsx(styles.mark, 'dflex', 'acenter', 'jcenter', 'cur')} key={item.id}>
+            className={clsx(styles.mark, 'dflex', 'acenter', 'jcenter', 'cur')} key={item.id}>
             <div className={clsx(styles.glass, 'dflex', 'acenter', 'jcenter')}>
                 <a href={item.src} rel='noreferrer' target='_blank'
-                   onClick={(e) => isManagement ? e.preventDefault() : ''}
-                   className={clsx('dflex', 'acenter', 'jcenter', 'flexcolumn', 'positionrelative')}>
+                    onClick={(e) => isManagement ? e.preventDefault() : ''}
+                    className={clsx('dflex', 'acenter', 'jcenter', 'flexcolumn', 'positionrelative')}>
                     <div className={clsx(styles.cardLogo, 'dflex', 'acenter')}>
                         <Image alt={'logo'} width={60} height={60} className={clsx(styles.cardImage, 'rounded')} src={item.logo!} />
                     </div>
